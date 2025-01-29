33fbb771db118978ddb84da3e4e87e8dccc6e554
Juan Pinheiro de França <rm552202@fiap.com.br>
2025-01-29 16:12:14 -0300

    Criando o Executável

---
+++ SepararCampanha.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import os
+
+def clean_phone_number(phone):
+    if pd.isna(phone):
+        return None
+    # Remove +55 e caracteres não numéricos
+    cleaned = str(phone).replace('+55', '').replace(' ', '')
+    cleaned = ''.join(filter(str.isdigit, cleaned))
+    return cleaned if cleaned else None
+
+def main():
+    # Criar/limpar diretório data
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    if 'data' in os.listdir():
+        for file in os.listdir('data'):
+            try:
+                os.remove(f'data/{file}')
+            except:
+                pass
+
+    # Ler o arquivo CSV
+    df = pd.read_csv('campanha.csv', 
+                     skiprows=[0], 
+                     usecols=['Nome','Campanha','Email','Telefone'])
+
+    # Tratar os telefones
+    # Primeiro, dividir os telefones por ';'
+    telefones_split = df['Telefone'].str.split(';', expand=True)
+    
+    # Limpar cada coluna de telefone
+    for col in telefones_split.columns:
+        telefones_split[col] = telefones_split[col].apply(clean_phone_number)
+    
+    # Garantir que a primeira coluna de telefone vá para 'Telefone'
+    df['Telefone'] = telefones_split[0]
+    
+    # Renomear as outras colunas como Telefone1, Telefone2, etc.
+    telefones_adicionais = telefones_split.iloc[:, 1:]
+    telefones_adicionais.columns = [f'Telefone{i+1}' for i in range(len(telefones_adicionais.columns))]
+    
+    # Concatenar com o DataFrame original
+    df = pd.concat([df, telefones_adicionais], axis=1)
+    
+    # Usar o primeiro telefone como ID
+    df['ID'] = df['Telefone']
+    
+    # Mover a coluna ID para a primeira posição
+    colunas = ['ID'] + [col for col in df.columns if col != 'ID']
+    df = df[colunas]
+    
+    # Limpar nome da campanha
+    df['Campanha'] = df['Campanha'].str.replace(r'[^\w\s]', '', regex=True)
+    
+    # Criar arquivos por campanha
+    list_campaing = df['Campanha'].unique()
+    for campaing in list_campaing:
+        x = df[df['Campanha'] == campaing]
+        x.to_excel(f'data/{campaing}.xlsx', index=False)
+    
+    if 'nan.xlsx' in os.listdir('data'):
+        os.remove('data/nan.xlsx')
+
+if __name__ == '__main__':
+    main()
---
+++ src/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+def main():
+    df = pd.re('relatorio_deals_Comparecimento.xlsx', usecols=['name_negociação','campaing_name','contact_email','contact_phone'])
+    df.rename(columns={
+        'name_negociação': 'Nome',
+        'campaing_name': 'Campanha',
+        'contact_email': 'Email',
+        'contact_phone': 'Telefone'
+    }, inplace=True)
+    #Fazendo a tratativa dos telefones
+    df['Telefone'] = df['Telefone'].str.replace(r'\+55', '', regex=True)
+    df['Telefone'] = df['Telefone'].str.replace(r'[^\d]', '', regex=True)
+    df['ID'] = df['Telefone']
+
+    df['Campanha'] = df['Campanha'].str.replace(r'[^\w\s]', '', regex=True)
+    list_campaing = df['Campanha'].unique()
+
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    for campaing in list_campaing:
+        x = df[df['Campanha'] == campaing]
+        x.to_excel(f'data/{campaing}.xlsx', index=False)
+    
+if __name__ == '__main__':
+    main()
